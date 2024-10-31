5b6859c2edc236e729658e88544a5f49d866a6fc
Marcos9868 <marcos_m_ferreira@outlook.com>
2024-10-30 22:00:32 -0300

    Feat: adiciona a configuração do DbContext

--- ApiCatalogo/Program.cs
+++ ApiCatalogo/Program.cs
@@ -1,8 +1,17 @@
+using ApiCatalogo.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+string connection = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddDbContext<DataContext>(options => 
+{
+    options.UseNpgsql(
+        connection
+    );
+});
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
