1343501e64b216710642a29cc7c96b6d9a1fdecd
Marcos9868 <marcos_m_ferreira@outlook.com>
2024-10-30 21:59:41 -0300

    Feat(add): adiciona o context na API

---
+++ ApiCatalogo/Data/DataContext.cs
@@ -0,0 +1,13 @@
+using ApiCatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiCatalogo.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {}
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
