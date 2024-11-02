7b1f5f77d73f66f5cab47a2ae93e41b77909c171
Marcos9868 <marcos_m_ferreira@outlook.com>
2024-11-01 22:02:38 -0300

    Feat: adiciona o relacionamento entre as entidades categoria e produto

--- ApiCatalogo/Migrations/20241102001016_AdicionaRelacionamentoCategoriaProduto.Designer.cs
+++ ApiCatalogo/Migrations/20241102001016_AdicionaRelacionamentoCategoriaProduto.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using ApiCatalogo.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace ApiCatalogo.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20241102001016_AdicionaRelacionamentoCategoriaProduto")]
+    partial class AdicionaRelacionamentoCategoriaProduto
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
