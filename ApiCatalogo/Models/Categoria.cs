ef00e453dfae91bd336c2649ebe707f351458596
Marcos9868 <marcos_m_ferreira@outlook.com>
2024-11-01 22:01:41 -0300

    Feat: adiciona annotatios na model e relaciona com produto

--- ApiCatalogo/Models/Categoria.cs
+++ ApiCatalogo/Models/Categoria.cs
@@ -1,9 +1,22 @@
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+
 namespace ApiCatalogo.Models
 {
     public class Categoria
     {
+        public Categoria()
+        {
+            Produtos = new Collection<Produto>();
+        }
+        [Key]
         public int Id { get; set; }
+        [Required]
+        [StringLength(80)]
         public string Nome { get; set; } = string.Empty;
+        [Required]
+        [StringLength(300)]
         public string ImagemUrl { get; set; } = string.Empty;
+        public ICollection<Produto> Produtos { get; set; }
     }
 }
