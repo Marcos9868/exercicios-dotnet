a281011f2c2f282296041fda4e737e69894d6d3d
Marcos9868 <marcos_m_ferreira@outlook.com>
2024-11-04 22:01:28 -0300

    Feat: adiciona esqueleto dos controladores da API

---
+++ ApiCatalogo/Controllers/CategoriasController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiCatalogo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoriasController : ControllerBase
+    {
+        
+    }
+}
