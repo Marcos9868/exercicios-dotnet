dd1f522044f5beb24316e10f01d4b6a22514f741
Marcos9868 <marcos_m_ferreira@outlook.com>
2024-11-06 21:24:20 -0300

    feat: adiciona os controladores de Produto e Categoria

--- ApiCatalogo/Controllers/CategoriasController.cs
+++ ApiCatalogo/Controllers/CategoriasController.cs
@@ -1,4 +1,7 @@
+using ApiCatalogo.Data;
+using ApiCatalogo.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ApiCatalogo.Controllers
 {
@@ -6,6 +9,51 @@ namespace ApiCatalogo.Controllers
     [Route("api/[controller]")]
     public class CategoriasController : ControllerBase
     {
-        
+        private readonly DataContext _context;
+        public CategoriasController(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var categorias = await _context.Categorias.Include(p => p.Produtos).ToListAsync();
+            if (categorias == null) return NotFound();
+            return Ok(categorias);
+        }
+        [HttpGet("{id:int}", Name = "ObterCategoria")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var categoria = await _context.Categorias.FirstOrDefaultAsync(p => p.Id == id);
+            if (categoria == null) return NotFound("categoria não encontrado");
+            return Ok(categoria);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post(Categoria categoria)
+        {
+            if (categoria == null) return BadRequest();
+            _context.Categorias.Add(categoria);
+            await _context.SaveChangesAsync();
+            return new CreatedAtRouteResult("Obtercategoria", new { id = categoria.Id}, categoria);
+        }
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> Put(int id, Categoria categoria)
+        {
+            var categoriaABuscar = await _context.Categorias.FirstOrDefaultAsync(p => p.Id == id);
+            if (categoriaABuscar == null) return NotFound();
+            _context.Entry(categoria).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return Ok(categoria);
+        }
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var categoria = await _context.Categorias.FirstOrDefaultAsync(p => p.Id == id);
+            if (categoria == null) return NotFound("categoria não encontrado");
+            _context.Categorias.Remove(categoria);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
     }
 }
--- ApiCatalogo/Controllers/ProdutosController.cs
+++ ApiCatalogo/Controllers/ProdutosController.cs
@@ -1,4 +1,7 @@
+using ApiCatalogo.Data;
+using ApiCatalogo.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ApiCatalogo.Controllers
 {
@@ -6,6 +9,50 @@ namespace ApiCatalogo.Controllers
     [Route("api/[controller]")]
     public class ProdutosController : ControllerBase
     {
-        
+        private readonly DataContext _context;
+        public ProdutosController(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var produtos = await _context.Produtos.ToListAsync();
+            if (produtos == null) return NotFound();
+            return Ok(produtos);
+        }
+        [HttpGet("{id:int}", Name = "ObterProduto")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var produto = await _context.Produtos.FirstOrDefaultAsync(p => p.Id == id);
+            if (produto == null) return NotFound("Produto não encontrado");
+            return Ok(produto);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post(Produto produto)
+        {
+            if (produto == null) return BadRequest();
+            _context.Produtos.Add(produto);
+            await _context.SaveChangesAsync();
+            return new CreatedAtRouteResult("ObterProduto", new { id = produto.Id}, produto);
+        }
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> Put(int id, Produto produto)
+        {
+            var produtoABuscar = await _context.Produtos.FirstOrDefaultAsync(p => p.Id == id);
+            if (produtoABuscar == null) return NotFound();
+            _context.Entry(produto).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return Ok(produto);
+        }
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var produto = await _context.Produtos.FirstOrDefaultAsync(p => p.Id == id);
+            if (produto == null) return NotFound("Produto não encontrado");
+            _context.Produtos.Remove(produto);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
     }
 }
